fb5e522d2e4cfb74e5990132a63ffe0c77f29351
chandan005 <phoenix.com005@gmail.com>
2016-04-12 17:01:31 +1000

    update

--- A*.py
+++ A*.py
@@ -1,67 +1,48 @@
-import heapq
-import math
+from heapq import heappush, heappop, heapify
+from collections import deque
 
 class Agent(object):
     def __init__(self,**kwargs):
-        self.openList = []
-        self.closedSet = set()
-        self.came_from = {}
-        self.path = []
-        self.call = True
-        
-
-    # method to reconstruct the path from the parents dict
-    def reconstruct(self,came_from,goal):
-        node = goal
-        self.path = [node]
-        while node in came_from:
-            node = came_from[node]
-            self.path.append(node)
-        return self.path
-
-
-    # returns a step towards the goal using A Star algorithm
-    def getNext(self, mapref, current, goal, timeremaining):
-        c = (64,215)
-        for move in mapref.getAllAdjacents(c):
-            if mapref.isPassable(move,(64,214)):
-                print move
-        if self.call == True:
-            self.call = False
-            heapq.heappush(self.openList,(mapref.getH(current,goal),0,current))
-            while goal not in self.closedSet:
-                _,cost,position = heapq.heappop(self.openList)
-            
-                if position == goal:
-                    print "Goal Found"
-                    self.reconstruct(self.came_from,goal)
-                    break
-            
-                if position in self.closedSet:
-                    continue
-                self.closedSet.add(position)
-                for move in mapref.getAllAdjacents(position):
-                    check = mapref.isPassable(move,position)
-
-                    if check == True:
-                        
-
-                        if move in self.closedSet:
-                            continue
-                        newCost = mapref.getCost(position,move)
-                
-                        if (move not in self.closedSet or newCost < cost):
-                            self.came_from[move] = position
-                            cost = newCost
-                            p = cost + mapref.getH(goal,move)
-                            heapq.heappush(self.openList,(p,newCost,move))
-                               
-            print "No of nodes in OPEN: %d" % (len(self.openList))
-            return self.path.pop()
-        else:
-            return self.path.pop()
-        return None
+        self.clockface = ((1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1))
 
     
+
+    def getNext(self, mapref, current, goal, timeremaining): 
+        queue = [(0, current, 0, None)]
+        enqueued = {}
+        explored = {}
+        while queue:
+            _, current_node, dist, parent = heappop(queue)
+            if current_node == goal:
+                cs = self.reconstruct(current_node,parent,explored)
+                next(cs)
+                break
+            if current_node in explored:
+                continue
+            explored[current_node] = parent
+            for move in mapref.getAllAdjacents(current_node):
+                if move in explored:
+                    continue
+                ncost = dist + mapref.getCost(current_node,move)
+                if move in enqueued:
+                    qcost, h = enqueued[move]
+                    if qcost <= ncost:
+                        continue
+                else:
+                    h = mapref.getH(goal,move)
+                enqueued[move] = ncost, h
+                heappush(queue,(ncost+h, move, ncost, current_node))
+        return next(cs)
+
+    def reconstruct(self,curnode,parent,explored):
+        path = [curnode]
+        node = parent
+        while node is not None:
+            path.append(node)
+            node = explored[node]
+        path.reverse()
+        for a in path:
+            yield a
+
     def reset(self, **kwargs):
         pass
